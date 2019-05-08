d1068dae27a25b440e34da45b2a2b48790bea392
Arif <arif.cvc13@gmail.com>
2019-05-08 14:18:07 -0500

    Add number pad fuctionality and click

--- First App/MainWindow.xaml.cs
+++ First App/MainWindow.xaml.cs
@@ -10,42 +10,132 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Animation;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace First_App
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        private List<string> listOfInput = new List<string>();
+
         public MainWindow()
         {
             InitializeComponent();
+            this.KeyDown += new KeyEventHandler(MainWindow_KeyDown);
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        void MainWindow_KeyDown(object sender, KeyEventArgs e)
         {
-            
+            int keyVal = (int)e.Key;
+            int value = -1;
+            if ((keyVal >= (int)Key.D0 && keyVal <= (int)Key.D9))
+            {
+                value = (int)e.Key - (int)Key.D0;
+            }
+            else if (keyVal >= (int)Key.NumPad0 && keyVal <= (int)Key.NumPad9)
+            {
+                value = (int)e.Key - (int)Key.NumPad0;
+            }
+            if (value != -1)
+            {
+
+                ShowDigits(value.ToString());
+            }
+
+            if (keyVal == 2)
+            {
+                RemoveChar();
+            };
+
         }
 
         private void ButtonCaptured(object sender, RoutedEventArgs e)
         {
-            var listOfInput = new List<string>();
+            var input = ((Button)sender).Tag.ToString();
 
-            var input = ((Button)sender).Tag;
-
-
-            //screen.Content = input;
-
-            listOfInput.Add(input.ToString());
-
-            foreach (var item in listOfInput)
+            if (input == "C")
             {
-                screen.Content += item;
+                RemoveChar();
+            }
+            else if (input == "AC" && input.Length > 0)
+            {
+                listOfInput.Clear();
+                screen.Content = "0";
+            }
+            else
+            {
+                ShowDigits(input);
+            }
+        }
+
+        private void ShowDigits(string input)
+        {
+            if (screen.Content.ToString() == "0")
+            {
+                screen.Content = "";
+            }
+
+            if (listOfInput.Count >= 13)
+            {
+                MessageBox.Show("Maximum Input exceed", "Input limit", MessageBoxButton.OK, MessageBoxImage.Stop);
+            }
+
+            else
+            {
+                listOfInput.Add(input);
+                screen.Content += input.ToString();
             }
 
         }
+
+        private void RemoveChar()
+        {
+            var contentString = screen.Content.ToString();
+
+            int lenghtOflebel = contentString.Count();
+
+            int inputLength = listOfInput.Count();
+
+            if (lenghtOflebel > 0 && inputLength > 0)
+            {
+                listOfInput.RemoveAt(inputLength - 1);
+                screen.Content = contentString.Remove(lenghtOflebel - 1);
+            }
+
+            if (listOfInput.Count == 0)
+            {
+                screen.Content = "0";
+            }
+        }
+
+
+        private void NavigateMenu(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation menuAnimation = new DoubleAnimation();
+
+            if (menus.Width == 0)
+            {
+                menuAnimation.From = 0;
+                menuAnimation.To = 206;
+            }
+            else
+            {
+                menuAnimation.From = 206;
+                menuAnimation.To = 0;
+            }
+
+            menuAnimation.Duration = new Duration(TimeSpan.FromSeconds(0.2));
+            menus.BeginAnimation(WidthProperty, menuAnimation);
+
+        }
+
+        private void SwitchOperation(object sender, RoutedEventArgs e)
+        {
+
+        }
+
     }
 }
