5e8f3227ca6ed97a113ddd8dcf4619da42d7706d
Arif <arif.cvc13@gmail.com>
2019-06-20 14:42:10 -0500

    :racehorse: change performance

---
+++ First App/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_App
+{
+    public interface ICalculator
+    {
+        string Calculate();
+    }
+
+    public class StandardCalculator : ICalculator
+    {
+        public string Calculate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DecimalCalculator : ICalculator
+    {
+        public string Calculate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CalculatorFactory
+    {
+        public static ICalculator Create(string param)
+        {
+            if (param == "standard")
+            {
+                return new StandardCalculator();
+            }
+
+            return null;
+        }
+    }
+
+    class Class1
+    {
+    }
+}
